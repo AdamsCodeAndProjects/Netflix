6eba3052ad45dfd45109f787311330dac2f26812
Adam Januszewski <Adpjan@att.net>
2022-06-21 15:01:02 -0400

    Initial Commit

---
+++ Netflix/NetflixApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetflixApp.swift
+//  Netflix
+//
+//  Created by adam janusewski on 6/21/22.
+//
+
+import SwiftUI
+
+@main
+struct NetflixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
